c310e008aad613bb891ec7d636b2727b7b29b35b
Thanh <123137981+AyKay8@users.noreply.github.com>
2023-03-06 17:07:35 -0500

    added storefront

---
+++ backend/components/__init__.py
@@ -0,0 +1,7 @@
+from composante import Composante
+
+composante1 = Composante("wwwwwwwwwwwwwwwwwwwwwwwwww")
+
+composante1.test()
+
+print(type(composante1))
---
+++ backend/components/component.py
@@ -0,0 +1,8 @@
+class Composante:
+
+    def __init__(self, a): #CONSTRUCTEUR
+        self.a = a
+
+    def test(self):
+        print(self.a)
+
