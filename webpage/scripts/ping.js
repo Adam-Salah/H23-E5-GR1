93d2083b32d4cc9e664b58d36991d373e2ff4673
Adam-Salah <2125976@bdeb.qc.ca>
2023-03-06 16:16:11 -0500

    updated main page

--- webpage/scripts/ping.js
+++ webpage/scripts/ping.js
@@ -7,3 +7,27 @@ function ping_function() {
         document.getElementById("pong").id = "ping";
     }
 }
+
+const banner_links = document.querySelectorAll('[id^=banner-link-]')
+for (let i = 0; i < banner_links.length; i++) {
+    banner_links[i].addEventListener("mouseenter", function () {mouseenter_link(this.id)})
+    banner_links[i].addEventListener("mouseleave", function () {mouseleave_link(this.id)})
+    banner_links[i].addEventListener("mousedown", function () {mousedown_link(this.id)})
+    banner_links[i].addEventListener("mouseup", function () {mouseup_link(this.id)})
+}
+
+function mouseenter_link(id) {
+    document.getElementById(id).style.backgroundColor = "grey";
+}
+
+function mouseleave_link(id) {
+    document.getElementById(id).style.backgroundColor = "black";
+}
+
+function mousedown_link(id) {
+    document.getElementById(id).style.fontSize = "3em";
+}
+
+function mouseup_link(id) {
+    document.getElementById(id).style.fontSize = "4em";
+}
