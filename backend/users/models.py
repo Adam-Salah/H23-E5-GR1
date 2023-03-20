eca53ce7d94bc82427a4068bc2c03ebcf6c4eddd
Thanh <123137981+AyKay8@users.noreply.github.com>
2023-03-19 22:23:34 -0400

    custom user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+'''
+Models in python are Classes which represent a table in a database
+Each model maps to a single table in a database
+https://docs.djangoproject.com/en/4.1/ref/models/fields/
+Everytime changes in databases : 
+python manage.py makemigrations
+python manage.py migrate
+'''
+
+'''
+(opens shell to interact with database)
+python manage.py shell 
+>>> from users.models import User 
+(from appName.modelsFile import ClassName inside models)
+>>> User.objects.all() (prints User table)
+
+>>> user1 = User(firstname='Tobias', lastname='Refsnes')
+>>> user2 = User(firstname='Linus', lastname='Refsnes')
+>>> user3 = User(firstname='Lene', lastname='Refsnes')
+>>> user4 = User(firstname='Stale', lastname='Refsnes')
+>>> user5 = User(firstname='Jane', lastname='Doe')
+>>> users_list = [user1, user2, user3, user4, user5]
+>>> for x in users_list:
+>>>   x.save()
+
+>>> User.objects.all().values() (check entire User table)
+>>> User.objects.all().delete() (delete entire table)
+>>> x = User.objects.all()[0] (x = user5)
+>>> x.firstname (print x's firstname)
+>>> x.firstname = "A"
+>>> x.save()
+>>> x.delete()
+
+>>> exit() (leaves shell)
+Everytime changes are made to the model : 
+python manage.py makemigrations users
+And then :
+python manage.py migrate
+
+'''
+
+class User(models.Model):
+    #firstname is a Character field
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    number = models.IntegerField(null=True)
+    joinedDate = models.DateField(null=True)
+    #null=True -> allows null values in the field
+
+    #just like toString
+    #CAREFUL : this has to be inside the User class (tab)
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,9 +1,53 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import User
+
 # Create your views here.
+'''
+    context['testing'] = "wadwadwad"
 
-def sayHello(request):
-    return HttpResponse('Hello World')
+    context = {
+        'testing': "testing",
+        'testing2': 123
+    }
 
-#def sayHello2(request):
-#    return HttpResponse('Hello World2')
+    #Passing a list
+    #instanciate list
+    listOfValues = []
+    listOfValues.append("first")
+    listOfValues.append("second")
+    context['listOfValues'] = listOfValues
+'''
+
+def usersScreen(request):
+    #creates allUsers object with values of the User model
+    users = User.objects.all().values()
+    #loads the allUsers.html template
+    template = loader.get_template('allUsers.html')
+
+    listOfValues = []
+    listOfValues.append("first")
+    listOfValues.append("second")
+    
+    context = {
+        #creates an object containing the allUsers object
+        #sends the object to the template
+        'users' : users,
+
+        'testing': "testing",
+        'testing2': 123,
+        'listOfValues': listOfValues
+    }
+    
+        #outpouts the HTML that is rendered by the template
+    return HttpResponse(template.render(context,request))
+
+def details(request, id):
+    #gets the id as an arugment
+    user = User.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'user': user,
+  }
+    return HttpResponse(template.render(context, request))
