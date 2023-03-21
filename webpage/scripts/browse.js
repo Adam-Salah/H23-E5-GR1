f9f65abe56d30838b86bd5a8d0e4f2d97e919ca2
Adam-Salah <2125976@bdeb.qc.ca>
2023-03-20 23:02:26 -0400

    fixed react problem

--- webpage/scripts/browse.js
+++ webpage/scripts/browse.js
@@ -1,9 +1,20 @@
-import ReactDOM from 'react-dom/client';
-import Project from "./Project";
+function Project() {
+
+    return (
+        <div>
+            <div></div>
+            <h2>Projet</h2>
+        </div>
+    );
+}
+
+let projects = [];
+for (let i = 0; i < 100; i++) {
+    projects.push(<Project/>)
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('view_tab'));
-for (let i = 0; i < 100; i++) {
-    console.log(i)
-    root.render(<Project />);
-}
+root.render(projects);
+
 
