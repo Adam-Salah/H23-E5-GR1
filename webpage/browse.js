2924a05f24618159938312bfe8fb3107dcb6789f
Adam-Salah <2125976@bdeb.qc.ca>
2023-03-27 17:43:39 -0400

    update browse.html color

--- webpage/browse.js
+++ webpage/browse.js
@@ -1,18 +1,19 @@
-function Project() {
-
+function Project(props) {
     return (
         <div>
-            <div></div>
-            <h2>Projet</h2>
+            <div style={{backgroundColor: props.color}}></div>
+            <h1>Projet</h1>
+            <h2>Date</h2>
+            <h3>Auteur</h3>
         </div>
     );
 }
 
 let projects = [];
 for (let i = 0; i < 100; i++) {
-    projects.push(<Project/>)
+    let color = Math.floor(Math.random()*16777215).toString(16);
+    projects.push(<Project color={"#"+color}/>)
 }
-
 
 const root = ReactDOM.createRoot(document.getElementById('view_tab'));
 root.render(projects);
